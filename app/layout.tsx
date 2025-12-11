036410e45a57523d44acdd5df4a2635a359070ae
Ritesh Singh <ritesh.davv@gmail.com>
2025-12-11 23:20:32 +0530

    Changed the default theme to dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
